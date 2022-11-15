06581940b4455273a33788b8f552bd6a11dbddd9
Paul LaCaille <paul@ionic.io>
2022-11-15 15:24:15 -0600

    add logic for BUILD_ENV

--- serverurl.sh
+++ serverurl.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 echo $SERVER_URL
+echo $BUILD_ENV
 
 envsubst < ./src/environments/environment.ts
 
 echo `cat ./src/environments/environment.ts`
+
+if [ "$BUILD_ENV" = "qa" ]; then
+    envsubst < ./src/environments/environment.ts
+    echo `cat ./src/environments/environment.ts`
+else
+    envsubst < ./src/environments/environment.prod.ts
+    echo `cat ./src/environments/environment.prod.ts`
+fi
